b3b22c8a041b5a92cb44927248d14bfe81e6b3f9
Igor <eltery@mail.ru>
2022-12-16 23:06:03 +0300

    First task corrected

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
-int CountNegativeNumbers(int n)
+int CountPositiveNumbers(int n)
 {  
 
     int count = 0;
@@ -9,7 +9,7 @@ int CountNegativeNumbers(int n)
         Console.Write("Введите положительное или отрицательное число: ");
         int number = Convert.ToInt32(Console.ReadLine());
         {
-            if(number < 0)
+            if(number > 0)
             count++;            
         }
         quantity++;
@@ -19,5 +19,5 @@ int CountNegativeNumbers(int n)
 
 Console.Write("Введите требуемое количество чисел для проверки: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int result = CountNegativeNumbers(num);
-Console.WriteLine("Количество введённых отрицательных чисел: " + result);
+int result = CountPositiveNumbers(num);
+Console.WriteLine("Количество введённых положительных чисел: " + result);
