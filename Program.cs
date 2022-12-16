8b9d0a4dc625d4224f8a462fd00b8d1218fe632e
Igor <eltery@mail.ru>
2022-12-16 22:52:31 +0300

    Negative numbers count done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
 ﻿
+int CountNegativeNumbers(int n)
+{  
+
+    int count = 0;
+    int quantity = 1;
+    while(quantity <= n)
+    {   
+        Console.Write("Введите положительное или отрицательное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        {
+            if(number < 0)
+            count++;            
+        }
+        quantity++;
+    }
+    return count;
+}
+
+Console.Write("Введите требуемое количество чисел для проверки: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = CountNegativeNumbers(num);
+Console.WriteLine("Количество введённых отрицательных чисел: " + result);
