f92582ce9e23501eb2e16b8d171a639c9b4d1ff9
Igor <eltery@mail.ru>
2022-12-17 20:05:27 +0300

    Точка пересечения прямых done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
-﻿
+﻿/*
 int CountPositiveNumbers(int n)
 {  
-
     int count = 0;
     int quantity = 1;
     while(quantity <= n)
@@ -21,3 +20,39 @@ Console.Write("Введите требуемое количество чисел
 int num = Convert.ToInt32(Console.ReadLine());
 int result = CountPositiveNumbers(num);
 Console.WriteLine("Количество введённых положительных чисел: " + result);
+*/
+
+
+void DotOfCross(double k1, double b1, double k2, double b2)
+{
+    if(k1 == k2 && b1 != b2)
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+
+    if(k1 == k2 && b1 == b2)
+    {
+            Console.WriteLine("Прямые совпадают");
+                
+    }
+    
+    if(k1 != k2 && b1 != b2)
+    {
+        double x = (b1 - b2) / (k2 - k1);
+        double y = (k1 * (b1 -b2) / (k2 - k1)) + b1;
+        Console.WriteLine($"Координаты точки пересечения прямых ({x};{y})");
+    }
+}
+
+Console.WriteLine("Чтобы найти точку пересечения двух прямых y=k1*x+b1 и y=k2*x+b2");
+Console.Write("Введите k1: ");
+double a1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double t1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double a2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double t2 = Convert.ToDouble(Console.ReadLine());
+DotOfCross(a1, t1, a2, t2);
+
+
